966c1494a38c8db9ce08a5ef6b98feb98296c22c
lchang2190 <lchang2190@gmail.com>
2017-11-29 21:59:50 -0800

    work in progress functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  _.extend(obj, stackMethods);
+  obj.count = 0;
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  pop: function() {
+    this.count++;
+  },
+
+  push: function(value) {
+  },
+
+  size: function() {
+    
+    return this.count;
+  }
+
+};
+
+
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,9 +1,41 @@
 var Stack = function() {
   var someInstance = {};
+  var count = 0;
+  // Use an object with numeric keys to store values
+  var storage = {};
+  
+  // Implement the methods below
+  someInstance.push = function(value) {
+    var index = someInstance.size() + 1;
+    storage[index] = value;
+    count++;
+    return storage;
+  };
+
+  someInstance.pop = function() {
+    var index = someInstance.size();
+    var del = storage[index];
+    delete storage[index];
+    if ( count > 0) {
+      count--;
+    }
+    return del;
+  };
+
+  someInstance.size = function() {
+    return count;
+  };
+
+  return someInstance;
+};
+
+
+/*var Stack = function() {
+  var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  
   // Implement the methods below
   someInstance.push = function(value) {
     var index = someInstance.size() + 1;
@@ -20,13 +52,11 @@ var Stack = function() {
 
   someInstance.size = function() {
     var count = 0;
-    for (var prop in storage){
+    for (var prop in storage) {
       count++;  
     }
     return count;
   };
 
   return someInstance;
-};
-
-
+};*/
