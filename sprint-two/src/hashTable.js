9c666c07fd0b2f186281a7b188a033672f105464
lchang2190 <lchang2190@gmail.com>
2017-12-02 17:42:48 -0800

    half of hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,23 +7,51 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return index;
+  //set up key value pair
+  //check index if undefined
+    // if undefined, create bucket;
+    //insert tuple to bucket;
+  let tuple = [];
+  tuple[0] = k;
+  tuple[1] = v;
+  if (this._storage.get(index) === undefined) {
+    var bucket = [];
+    bucket.push(tuple);
+    this._storage.set(index, bucket);
+  }
+ 
+  if (this._storage.get(index) !== undefined) {
+    
+    this._storage.get(index).push(tuple);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //use get() to use the index to get the key:val
   
+  return this._storage.get(index)[1][1];
+    
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //check to see if index has buckets
+    //if index has buckets
+    //
+  this._storage.each(function(pos) {
+    if (pos !== undefined) {
+      for (var i = 0; i < pos.length; i++) {
+        if (pos[i].indexOf(k)) {
+          pos[i] = undefined;
+        }
+      }
+    }
+  });
+  
 };
 
 var hash = new HashTable();
-console.log(hash.insert(a, 1));
-
-
-
 
 
 /*
